f5e97c44d4054b097b4f7bb27c380ef196e86046
Eric Trombly <etrombly@yahoo.com>
2017-05-30 09:23:20 +0200

    change to 4 steps instead of 8

--- src/stepper/ulnXXXX.rs
+++ src/stepper/ulnXXXX.rs
@@ -2,6 +2,7 @@ use hal::pin::{Pin, State};
 pub use ::stepper::Stepper as halStepper;
 pub use ::stepper::Direction;
 
+/*
 const STEPS:[[State; 4]; 8] = [[State::HIGH,State::HIGH,State::LOW,State::LOW],
                 [State::LOW,State::HIGH,State::LOW,State::LOW],
                 [State::LOW,State::HIGH,State::HIGH,State::LOW],
@@ -10,6 +11,12 @@ const STEPS:[[State; 4]; 8] = [[State::HIGH,State::HIGH,State::LOW,State::LOW],
                 [State::LOW,State::LOW,State::LOW,State::HIGH],
                 [State::HIGH,State::LOW,State::LOW,State::HIGH],
                 [State::HIGH,State::LOW,State::LOW,State::LOW],];
+*/
+
+const STEPS:[[State; 4]; 4] = [[State::HIGH,State::HIGH,State::LOW,State::LOW],
+                [State::LOW,State::HIGH,State::HIGH,State::LOW],
+                [State::LOW,State::LOW,State::HIGH,State::HIGH],
+                [State::HIGH,State::LOW,State::LOW,State::HIGH],];
 
 pub struct Stepper<'a, T> 
 where T: 'a{
@@ -27,22 +34,22 @@ impl<'a, T> halStepper for Stepper<'a, T>{
     }
 
     fn step(&mut self){
-        let index = self.index;
-        match self.direction {
-            Direction::RIGHT => {if self.index <7 {
-                    self.index += 1;
-                } else {
-                    self.index = 0;
-                }},
-            Direction::LEFT => {if self.index > 0 {
-                    self.index -= 1;
-                } else {
-                    self.index = 7;
-                }},
-        }
         self.pin1.digital_write(&STEPS[index as usize][0]);
         self.pin2.digital_write(&STEPS[index as usize][1]);
         self.pin3.digital_write(&STEPS[index as usize][2]);
         self.pin4.digital_write(&STEPS[index as usize][3]);
+
+        let self.index = match self.direction {
+            Direction::RIGHT => {if self.index < 3 {
+                    self.index + 1
+                } else {
+                    0
+                }},
+            Direction::LEFT => {if self.index > 0 {
+                    self.index - 1
+                } else {
+                    3
+                }},
+        }
     }
 }
